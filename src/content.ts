b13484aa331b8f37584a84c73884ed318aa6babb
Ori <sdgwsld@gmail.com>
2026-03-08 01:19:49 +0900

    chore(shared): 진입점 부트스트랩과 todo를 정리한다

--- src/content.ts
+++ src/content.ts
@@ -1,2 +1,10 @@
+import { ACTIVATION_BUBBLE_THRESHOLD } from './shared/constants.ts'
+import { parseTranscriptPath } from './shared/routes.ts'
+
+const transcriptPath = parseTranscriptPath(window.location.pathname)
+
 // 콘텐츠 스크립트 진입점
-console.log('cgpt-virtualizer content loaded');
+console.log('cgpt-virtualizer content loaded', {
+  activationBubbleThreshold: ACTIVATION_BUBBLE_THRESHOLD,
+  transcriptPath,
+})
--- src/worker.ts
+++ src/worker.ts
@@ -1,2 +1,18 @@
+import {
+  createGetTabStatusMessage,
+  createTabStatusMessage,
+  type PopupToWorkerMessage,
+  type WorkerToPopupMessage,
+} from './shared/messages.ts'
+import { createRuntimeStatus } from './shared/types.ts'
+
+const bootstrapRequest: PopupToWorkerMessage = createGetTabStatusMessage()
+const bootstrapResponse: WorkerToPopupMessage = createTabStatusMessage(
+  createRuntimeStatus('Off'),
+)
+
 // 서비스 워커 진입점
-console.log('cgpt-virtualizer worker loaded');
+console.log('cgpt-virtualizer worker loaded', {
+  bootstrapRequest,
+  bootstrapResponse,
+})
