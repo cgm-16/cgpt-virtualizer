1afa38e72109d2d0584dbb0351f84d1fa96fa1e4
Ori <sdgwsld@gmail.com>
2026-04-23 09:34:53 +0900

    chore: changed gitignore, agent context, exposed codex-memories to github

---
+++ QA.md
@@ -0,0 +1,1 @@
+2020. 3.  9.  - The features do not work at all, always `Unavailable` or `Off` initialy
--- eslint.config.ts
+++ eslint.config.ts
@@ -13,5 +13,5 @@ export default defineConfig([
   },
   tseslint.configs.recommended,
   eslintConfigPrettier,
-  globalIgnores(["dist/"]),
+  globalIgnores(["dist/", ".worktrees"]),
 ]);
